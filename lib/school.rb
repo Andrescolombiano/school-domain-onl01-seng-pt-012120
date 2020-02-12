ff93da940a81920c03e769b7f7518b9a9c7328a2
Andres Carvajal <jc1993box@hotmail.com>
2020-02-12 05:56:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,11 +10,16 @@ class School
  def add_student (name, grade)
    @roster[grade]||=[]
    @roster[grade] << name
-end
+ end
    
   def grade(grade)
     @roster[grade]
  end
   
-  def
+  def sorted (grade, name)
+    sorted = {}
+    sorted.each do |grade, name|
+      sorted.sort
+  end
+  sorted
 end
