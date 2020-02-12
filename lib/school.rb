d3fe24298334573592d4c9dd3205e79569dc229c
Andres Carvajal <jc1993box@hotmail.com>
2020-02-12 05:31:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School 
+  
+  attr_accessor :reader, :roster
+  
+ def initialize(name)
+  @name=name
+  roster = {}
+ end
+
+ def add_student(name, grade)
+   {#{grade} =. [#"name"]
+end
+  
+  
+ school = School.new("Bayside High School")
